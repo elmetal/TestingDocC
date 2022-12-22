b3e1e15edb9149f0e55c5052390b2e183cf01037
elmetal <nm7.pupu@gmail.com>
2022-12-22 17:51:15 +0900

    package init

---
+++ Tests/TestingDocCTests/TestingDocCTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import TestingDocC
+
+final class TestingDocCTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(TestingDocC().text, "Hello, World!")
+    }
+}
