0ba0ab885e24484f1d60beb1375b4ce66bdb838c
elmetal <nm7.pupu@gmail.com>
2023-09-25 16:47:25 +0900

    Update Cat.swift

--- Sources/TestingDocC/Cat.swift
+++ Sources/TestingDocC/Cat.swift
@@ -1,15 +1,15 @@
 /// A cat.
-struct Cat {
+public struct Cat {
     /// The name of the cat.
-    let name: String
+    public let name: String
 
     /// The age of the cat.
-    let age: Int
+    public let age: Int
 
     /// The breed of the cat.
     /// - Note: This is an optional value. If the cat is a mutt, this value will be `nil`.
-    let breed: String?
+    public let breed: String?
 
     /// Makes the cat meow.
-    func meow() { print("Meow!") }
+    public func meow() { print("Meow!") }
 }
